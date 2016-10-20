56d4539c3ceb18d966274452b4434c8b501f3b25
masuhajime <masuhajime@gmail.com>
2016-10-20 14:51:47 +0000

    .bash_profileへの自動追記と冪等性

--- setup.sh
+++ setup.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-echo "### setup start ###"
+// shell 冪等性
+// http://qiita.com/yn-misaki/items/3ec0605cba228a7d5c9a
 
-# git completion
-curl -o ~/dotfiles/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-mv ~/dotfiles/git-completion.bash ~/dotfiles/.git-completion.bash
-cp ~/dotfiles/.gitconfig.sample ~/dotfiles/.gitconfig
+DIR_DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "### setup start"
+echo "===="
+
+echo "### curl git-completion.bash"
+curl -o "$DIR_DOTFILES/.git-completion.bash" https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+
+# .gitconfig
+if [ ! -e "$DIR_DOTFILES/.gitconfig" ]; then
+  cp /dotfiles/.gitconfig.sample ~/dotfiles/.gitconfig
+fi
 
 echo "### cleate links ###"
 create_link () {
-    cmd="ln -nfs $HOME/dotfiles/$1 $HOME/$file"
-    $cmd
-    echo $cmd
+  cmd="ln -nfs $DIR_DOTFILES/$1 $HOME/$file"
+  $cmd
+  echo $cmd
 }
 create_link ".git-completion.bash"
 create_link ".gitconfig"
@@ -23,12 +32,32 @@ cmd="curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/inst
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 echo $cmd
 
-echo "### setup end ###"
+echo "### done \(^o^)/"
+
+source_file() {
+source > /dev/null 2>&1
+  if [ $? -eq 127 ]; then
+    . "$1"
+  else
+    source "$1"
+  fi
+}
+
+BASH_PROFILE="$HOME/.bash_profile"
+FILE_DOTFILES_PROFILE="$DIR_DOTFILES/dotfiles_profile"
+TAG="##### dotfiles_profile"
+if [ -e $BASH_PROFILE ]; then
+  if [ ! "`grep $TAG $BASH_PROFILE`" ]; then
+    echo "add source $FILE_DOTFILES_PROFILE to $BASH_PROFILE"
+    echo "" >> "$BASH_PROFILE"
+    echo "$TAG" >> "$BASH_PROFILE"
+    echo ". $FILE_DOTFILES_PROFILE" >> "$BASH_PROFILE"
+    source_file "$BASH_PROFILE"
+  fi
+fi
 
 echo "### TODO ###"
-echo "$ echo \"source ~/.git-completion.bash\" >> ~/.bash_profile"
-echo "$ source ~/.bash_profile"
+echo "source $BASH_PROFILE"
 echo "git config --global user.name <name>"
 echo "git config --global user.email <email>"
-echo "############"
 
